b42a8b4e4f24db9426cfec7e5b743d1d446e779f
EdvanSilvaFilho <edvansilvafilho2@outlook.com>
2021-08-11 02:16:01 -0300

    Conversão de medidas de camisa Br/Usa

---
+++ Tamanho_das_camisas.js
@@ -0,0 +1,18 @@
+const tamanhoUsa = {
+  camisa1: 'PP',
+  camisa2: 'P',
+  camisa3: 'M',
+  camisa4: 'G',
+  camisa5: 'GG'
+}
+if (tamanhoBr === tamanhoUsa.camisa1) {
+  console.log('XS')
+} else if (tamanhoBr === tamanhoUsa.camisa2) {
+  console.log('S')
+} else if (tamanhoBr === tamanhoUsa.camisa3) {
+  console.log('M')
+} else if (tamanhoBr === tamanhoUsa.camisa4) {
+  console.log('L')
+} else {
+  console.log('XL')
+}
